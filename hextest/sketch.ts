b4ae072a90c3b88abe2a7c3cb40fbb92e0020314
Adrian Garza <adrianhgarza@gmail.com>
2024-03-04 21:02:33 -0800

    ok it works on windows now

--- hextest/sketch.ts
+++ hextest/sketch.ts
@@ -1,5 +1,15 @@
 let grid: HexGrid;
 
-void setup() {
+function setup(): void {
 	grid = new HexGrid(new vec2(400, 400), new vec2(600, 600), 100);
+	createCanvas(800, 800);
+}
+
+function draw(): void {
+	noFill();
+	background(200);
+	stroke(50);
+	strokeWeight(5);
+	translate(400, 400);
+	ellipse(0, 0, 20, 20);
 }
