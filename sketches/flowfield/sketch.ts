c64005ca51314f76371d7051bbb6f299eb7d7b41
adrian <adrianhgarza@gmail.com>
2024-06-09 00:53:12 -0700

    et

--- sketches/flowfield/sketch.ts
+++ sketches/flowfield/sketch.ts
@@ -1,10 +1,10 @@
 // import { Quadtree } from "@timohausmann/quadtree-ts"
 // import { Circle } from "@timohausmann/quadtree-ts";
 
-const pointSpacing = 10;
+const pointSpacing = 500;
 const noiseScale = 1;
-const lineSpacing = 3;
-const pointDistance = 5;
+const collisionSize = 10;
+const pointDistance = 100;
 
 const fieldTree = new Quadtree({
 	width: 800,
@@ -18,54 +18,71 @@ function setup(): void {
 	createCanvas(800, 800);
 	noLoop();
 	strokeWeight(5);
-	stroke(0xff0000);
+	stroke("red");
 	noFill();
+	angleMode(RADIANS);
+	strokeJoin(ROUND);
 }
 
 function draw(): void {
 	console.log("unga bunga!");
 	background(200);
-	for (let x=0; x<800; x+=pointSpacing) {
-		for (let y=0; y<800; y+=pointSpacing) {
+	for (let x=100; x<700; x+=pointSpacing) {
+		for (let y=100; y<700; y+=pointSpacing) {
+			ellipse(x, y, 50, 50);
 			// draw until it's either:
 			// 1. out of bounds
 			// 2. intersecting
 			let px = x;
 			let py = y;
-			beginShape(POINTS);
 			// don't intersect with self, keep a list of circles to add
 			// and do them at the end
 			let lineCollision: Array<Circle> = [];
-			while (canMakePoint(px, py)) {
+			let vertexCount = 0;
+			beginShape();
+			while (canMakePoint(px, py, ++vertexCount)) {
 				vertex(px, py);
 				// drop a circle in the quadtree for that point's intersection
 				lineCollision.push(new Circle({
 					x: px,
 					y: py,
-					r: lineSpacing
+					r: collisionSize
 				}));
 
 				// then sample noise to determine next direction
-				let ang = noise(px * noiseScale, py * noiseScale);
+				let ang = noise(px * noiseScale, py * noiseScale) * TWO_PI;
 				// move in the direction * 4 px
-				px += cos(ang) * 4;
-				py += sin(ang) * 4;
+				px += cos(ang) * pointDistance;
+				py += sin(ang) * pointDistance;
 			}
 			endShape();
 			// add the line to the collision quadtree
 			for (let i=0; i<lineCollision.length; i++) {
+				console.log("adding new vertex point")
 				fieldTree.insert(lineCollision[i]);
+				ellipse(lineCollision[i].x, lineCollision[i].y, collisionSize, collisionSize);
 			}
 		}
 	}
 }
 
-function canMakePoint(xPos: number, yPos: number): boolean {
+function canMakePoint(xPos: number, yPos: number, v: number): boolean {
+	console.log("checking "+xPos+", "+yPos+", "+collisionSize);
+	// TODO: this is returning everything??
+	// right beacuse the tree hasnt split yet
+	// ok so now just test those for collision. great
+	// do this tomorrow, fuck 
+	let overlap = fieldTree.retrieve(new Circle({
+		x: xPos,
+		y: yPos,
+		r: collisionSize
+	}));
+	
+	let hasOverlap: boolean = overlap.length > 0;
+	if (hasOverlap) console.log(overlap);
+
 	return xPos >= 0 && xPos <= 800
 		&& yPos >= 0 && yPos <= 800
-		&& (fieldTree.retrieve(new Circle({
-			x: xPos,
-			y: yPos,
-			r: pointSpacing
-		})).length == 0)
+		&& v < 50
+		&& !hasOverlap;
 }
